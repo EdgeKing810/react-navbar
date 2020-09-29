816585c589e46421f36425441197eabc47987084
Kishan Takoordyal <kt@esokia-webagency.com>
2020-09-29 14:30:38 +0400

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import useWindowSize from './useWindowSize';
+
+export default function App() {
+    const history = useHistory();
+    const { width } = useWindowSize();
+
+    const [showList, setShowList] = useState(false);
+
+    const navElements = ['Sign Up', 'About', 'Contact Us', 'Services', 'Home'];
+    let listElements = navElements.map((el, i) => (
+        <div className="flex p-2 text-xl text-blue-100 mx-2 font-mono hover:bg-blue-400 rounded" key={`nav-${i}`}>{el}</div>
+    ))
+
+    const collapsedMobileList = navElements.map((el, i) => (
+        <div className="flex justify-center text-blue-900 p-2 text-xl font-mono bg-blue-400 border-b-2 border-blue-300 w-full text-center" key={`nav-${i}`}>{el}</div>
+    ));
+
+    if (width < 600) {
+        listElements = <button className="ri-menu-line text-blue-100 text-2xl" onClick={() => setShowList(prev => !prev)} />
+    }
+
+    return(
+        <div className="w-full">
+            <div className="w-full bg-blue-500 p-4 flex">
+                <button className="flex w-1/2 text-2xl font-bold text-blue-100 tracking-wider"
+                        onClick={() => history.push('/')}
+                >
+                    <p>React</p>
+                    <i className="ml-2 ri-reactjs-line"></i>
+                </button>
+                <div className="flex flex-row-reverse w-1/2">
+                    {listElements}
+                </div>
+            </div>
+            <div className={`flex flex-col-reverse transform ${showList ? 'translate-y-0 ease-out duration-500' : '-translate-y-screen ease-in duration-500 opacity-0'}`}>{collapsedMobileList}</div>
+        </div>
+    );
 }
-
-export default App;
